b8a1956d7cd8a317a963bd38307315fce3945007
nurahmetemin <nurahmedemin@gmail.com>
2022-09-24 18:20:43 -0700

    Stereotype Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
@@ -0,0 +1,9 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.cydeo")
+public class ConfigCourse {
+}
