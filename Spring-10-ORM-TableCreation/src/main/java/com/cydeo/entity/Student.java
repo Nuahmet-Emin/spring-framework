901f275b16b2c83b8bca9b89958e5eb402ed7088
nurahmetemin <nurahmedemin@gmail.com>
2023-04-28 09:40:30 -0700

    annotation @GeneratedTimeValue,@Table,@Column

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,17 +1,33 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+
+    @Transient
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthdate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthtime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
 
 
 }
