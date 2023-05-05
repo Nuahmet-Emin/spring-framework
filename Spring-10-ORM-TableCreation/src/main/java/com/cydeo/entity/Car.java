f0f6f8a697610e0ffc5b668a7cc18e93365ee6e0
nurahmetemin <nurahmedemin@gmail.com>
2023-05-04 22:51:37 -0700

    added car class and variables

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -0,0 +1,15 @@
+package com.cydeo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String make;
+    private String model;
+}
