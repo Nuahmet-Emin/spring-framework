cb1e6d7621ef502a3ed55c3109adab4d4feb6889
nurahmetemin <nurahmedemin@gmail.com>
2023-05-05 16:20:50 -0700

    adding car objects to tables

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,9 +1,18 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.entity.Car;
+import com.cydeo.repository.CarRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataGenerator implements CommandLineRunner {
+
+    CarRepository carRepository;
+
+    public DataGenerator(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -13,6 +22,10 @@ public class DataGenerator implements CommandLineRunner {
         Car c3 = new Car("Toyota","Camry");
 
         // save these object to db
+        carRepository.save(c1);
+        carRepository.save(c2);
+        carRepository.save(c3);
+
 
 
 
