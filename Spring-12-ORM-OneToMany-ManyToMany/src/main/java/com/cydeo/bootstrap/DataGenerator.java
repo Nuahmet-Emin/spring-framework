695be1bcff4ea6feadfd417c09b84c105d50e34c
nurahmetemin <nurahmedemin@gmail.com>
2023-06-09 21:57:58 -0700

    added injection for merchant in data generator

--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -14,9 +14,11 @@ import java.util.Arrays;
 public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
+    private final MerchantRepository merchantRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
         this.paymentRepository = paymentRepository;
+        this.merchantRepository = merchantRepository;
     }
 
     @Override
@@ -36,6 +38,9 @@ public class DataGenerator implements CommandLineRunner {
 
         payment1.setMerchant(merchant1);
         payment2.setMerchant(merchant1);
+
+        merchantRepository.save(merchant1);
+
 
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
