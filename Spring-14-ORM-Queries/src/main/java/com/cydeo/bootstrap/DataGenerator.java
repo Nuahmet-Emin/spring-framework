df9e612a37f7f153a842e664d88010153a2bee5a
nurahmetemin <nurahmedemin@gmail.com>
2023-08-20 23:23:29 -0700

    added DepartmentRepository and method in DataGenerator

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Component;
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public DataGenerator(RegionRepository regionRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
@@ -26,6 +29,19 @@ public class DataGenerator implements CommandLineRunner {
         System.out.println("findTopByCountry: " + regionRepository.findTop2ByCountry("Canada"));
 
         System.out.println("--------Region End---------");
+
+
+
+
+        System.out.println("--------Department Start---------");
+
+        System.out.println("findByDepartment: " + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivision: " + departmentRepository.findByDivisionIs("Outdoors"));
+        System.out.println("findDistinctTop3ByDivisionContains': " + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+
+
+        System.out.println("--------Department End---------");
 
     }
 }
