2b1645b65fc44581f95643370600ab54181477a0
nurahmetemin <nurahmedemin@gmail.com>
2023-08-24 22:52:01 -0700

    added more method with JPQL with parameter to EmployeeRepository.java

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
@@ -46,6 +47,12 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Query("SELECT e.salary from Employee e where e.email = 'sdubber7@t-online.de'")
     Integer getEmployeeSalary();
+
+    @Query("select e From Employee e where e.email =?1")
+    Optional<Employee> getEmployeeDetail(String email);
+
+    @Query("Select e From Employee e Where e.email =?1 and e.salary =?2 ")
+    Employee getEmployeeDetail(String email, int salary);
 
 
 
