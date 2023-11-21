3034dec1453ed48b471493e34063b6cddd123a0d
nurahmetemin <nurahmedemin@gmail.com>
2023-11-21 11:12:22 -0800

    added create courses api with header

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseControllerResponseEntity.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseControllerResponseEntity.java
@@ -1,13 +1,11 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
+import com.cydeo.entity.Course;
 import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +37,13 @@ public class CourseControllerResponseEntity {
                 .header("Version","Cydeo.V2")
                 .header("Operation","Get List")
                 .body(courseService.getCourseById(courseId));*/
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Operation","Create")
+                .body(courseService.createCourse(course));
     }
 }
