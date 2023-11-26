ce3283eea194e3c1ea7a68a8dfa4ad1d59e61ca5
nurahmetemin <nurahmedemin@gmail.com>
2023-11-26 11:31:05 -0800

    created Post and Delete api for genre

--- Spring-17-REST-ConsumingApis/Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_Webclient.java
+++ Spring-17-REST-ConsumingApis/Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_Webclient.java
@@ -1,13 +1,12 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.entity.Genre;
 import com.cydeo.entity.MovieCinema;
 import com.cydeo.repository.GenreRepository;
 import com.cydeo.repository.MovieCinemaRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -41,6 +40,20 @@ public class Consume_Webclient {
     public ResponseEntity<Mono<MovieCinema>>readById(@PathVariable("id") Long id){
 
         return ResponseEntity.ok(Mono.just(movieCinemaRepository.findById(id).get()))    ;
+    }
+
+    @PostMapping("/create-genre")
+    public Mono<Genre> createGenre(@RequestBody Genre genre){
+        Genre createdGenre = genreRepository.save(genre);
+
+       return Mono.just(createdGenre);
+    }
+
+    @DeleteMapping("/delete/genre/{id}")
+    public Mono<Void> deleteGenre(@PathVariable("id") Long id){
+        genreRepository.deleteById(id);
+
+       return Mono.empty();
     }
 
 }
