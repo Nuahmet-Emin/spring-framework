1424f804fb9d70143b2b852e84d3dfac806985d6
nurahmetemin <nurahmedemin@gmail.com>
2024-01-23 22:44:50 -0800

    added new module for AOP

---
+++ Spring-20-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -0,0 +1,20 @@
+package com.cydeo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Aspect
+@Configuration
+public class LoggingAspect {
+
+
+
+
+}
